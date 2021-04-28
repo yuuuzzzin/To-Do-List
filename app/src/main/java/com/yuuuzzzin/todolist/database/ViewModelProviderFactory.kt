045469fc1f03237fe4c93f9e570a3787a6ba82dc
yuuuzzzin <uiuiui118437@gmail.com>
2021-04-29 03:23:38 +0900

    ViewModelProviderFactory 생성

---
+++ app/src/main/java/com/yuuuzzzin/todolist/database/ViewModelProviderFactory.kt
@@ -0,0 +1,16 @@
+package com.yuuuzzzin.todolist.database
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import java.lang.IllegalArgumentException
+
+class ViewModelProviderFactory(val context: Context) : ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(TodoViewModel::class.java)) {
+            TodoViewModel(context) as T
+        } else {
+            throw IllegalArgumentException()
+        }
+    }
+}
