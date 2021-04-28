3526d9480d3fe6b8d3e8c08b38227a5a6ffd16e8
yuuuzzzin <uiuiui118437@gmail.com>
2021-04-29 03:22:12 +0900

    Todo 엔티티 클래스 생성

---
+++ app/src/main/java/com/yuuuzzzin/todolist/database/Todo.kt
@@ -0,0 +1,13 @@
+package com.yuuuzzzin.todolist.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity
+data class Todo(var text: String?, var isCompleted: Boolean = false) : Serializable {
+    // 1. 엔티티 클래스 생성
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+}
